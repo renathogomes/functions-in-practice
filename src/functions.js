1e930b86c335e88288c1ad3ef2aad3dc4eaad39e
renathogomes <renatho.20gomes@gmail.com>
2023-04-05 00:45:49 -0300

    tentativa de arrow, sem sucesso

--- src/functions.js
+++ src/functions.js
@@ -1,6 +1,10 @@
 // 1 - Crie a função ligarDesligar, que ligue e desligue um motor de um carro.
+let status = 'ligado'
+const ligarDesligar = (status) => ((status === 'ligado') ?
+  (console.log(`O motor esta desligado`), `desligado`) :
+  (console.log(`O motor esta ligado`), `ligado`));
 
-
+console.log(ligarDesligar(status))
 // 2 - Crie a função circleArea, que calcule a área de um círculo. 
 
 
@@ -9,7 +13,13 @@
 
 // Não modifique as linhas abaixo
 module.exports = {
-  ligarDesligar: typeof ligarDesligar === 'function' ? ligarDesligar : (() => {}),
-  circleArea: typeof circleArea === 'function' ? circleArea : (() => {}),
-  longestWord: typeof longestWord === 'function' ? longestWord : (() => {}),
+  ligarDesligar: typeof ligarDesligar === 'function' ? ligarDesligar : (() => { }),
+  circleArea: typeof circleArea === 'function' ? circleArea : (() => { }),
+  longestWord: typeof longestWord === 'function' ? longestWord : (() => { }),
 };
+
+
+
+
+
+
