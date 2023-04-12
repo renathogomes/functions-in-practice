20e72eff71339a5c4e3045c02e0f38dc4f4d439c
renathogomes <renatho.20gomes@gmail.com>
2023-04-12 05:56:19 -0300

    segunda finalizada

--- src/functions.js
+++ src/functions.js
@@ -1,28 +1,26 @@
 // 1 - Crie a função ligarDesligar, que ligue e desligue um motor de um carro.
-/* let status = 'desligado'; */
-
-/* function ligarDesligar(status) {
-  if (status === 'ligado') {
-    console.log('O motor esta desligado');
-    return status = 'desligado';
-  } else {
-    console.log('O motor esta ligado');
-    return status = 'ligado';
-  }
-} */
-/* const ligarDesligar = (status) => (status === 'ligado') ?
-  (console.log('O motor esta desligado'), (status = 'desligado')) :
-  (console.log('O motor esta ligado'), (status = 'ligado'))); */
 
 
 const ligarDesligar = (status) => {
-status = status === 'ligado' ? 'desligado' : 'ligado';
-console.log(`O motor está ${status}`)
-return status
+  status = status === 'ligado' ? 'desligado' : 'ligado';
+  console.log(`O motor está ${status}`)
+  return status
 }
-
 console.log(ligarDesligar('desligado'));
+
 // 2 - Crie a função circleArea, que calcule a área de um círculo. 
+
+const circleArea = (raio) => {
+  const valorDePi = Number.parseFloat(Math.PI.toFixed(2));
+  const resultado = valorDePi * (raio ** 2);
+  if (typeof raio === typeof valorDePi) {
+    return `Essa é a área do círculo: ${resultado}`
+  } else {
+    return `O parâmetro radius deve ser um número`
+  }
+};
+
+console.log(circleArea(10))
 
 
 // 3 - Crie a função longestWord, que receba uma frase como parâmetro e retorne a maior palavra da frase.
